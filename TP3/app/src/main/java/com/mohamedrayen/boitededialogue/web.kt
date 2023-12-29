8e1d8cced05351ab83b9d6792f69e6f5bcdbb479
BhMedRayen <rayenmohamed2310@gmail.com>
2023-12-29 19:13:42 +0100

    TP3

---
+++ TP3/app/src/main/java/com/mohamedrayen/boitededialogue/web.kt
@@ -0,0 +1,11 @@
+package com.mohamedrayen.boitededialogue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class web : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web)
+    }
+}
