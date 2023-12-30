20d8da2dabaa17a68a7c4187cca89b62e5dc3e92
BhMedRayen <rayenmohamed2310@gmail.com>
2023-12-30 22:52:08 +0100

    TP4

---
+++ Recycler/app/src/main/java/com/mohamedrayen/recycler/MyAdapter.kt
@@ -0,0 +1,9 @@
+package com.mohamedrayen.recycler
+
+import androidx.recyclerview.widget.RecyclerView
+
+class Adapter(private val myDataSet: ArrayList<String>)
+    :RecyclerView.Adapter<MyA>
+    {
+
+}
