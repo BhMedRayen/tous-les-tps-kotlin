8e8a65244ef2d83d54e4cc1324e52fbb645b3182
BhMedRayen <rayenmohamed2310@gmail.com>
2024-01-04 20:46:28 +0100

    gradle edit

--- TP6/app/build.gradle.kts
+++ TP6/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.mohamedrayen.tpsharedpreferences"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.mohamedrayen.tpsharedpreferences"
--- TP6/app/src/main/java/com/mohamedrayen/tpsharedpreferences/MainActivity.kt
+++ TP6/app/src/main/java/com/mohamedrayen/tpsharedpreferences/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.mohamedrayen.tpsharedpreferences
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var savedData : ArrayList<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val name = findViewById<EditText>(R.id.name)
+        val password = findViewById<EditText>(R.id.password)
+        val login = findViewById<Button>(R.id.login)
+        val remember = findViewById<CheckBox>(R.id.checkBox)
     }
+    private fun saveInfoDate (savedData : ArrayList<String>) {
+        val sharedPreferences : SharedPreferences = getSharedPreferences("name", MODE_PRIVATE)
+        val save : SharedPreferences.Editor = sharedPreferences.edit()
+        save.putString("data", savedData.toString()).commit()
+    }
+    
 }
